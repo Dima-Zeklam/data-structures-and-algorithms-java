3a79a380c8d56d36d0eb14e242d4167696ec73e2
Dima Zeklam <dema.ziklam@gmail.com>
2021-10-23 00:23:04 +0300

    create stak and queue class

---
+++ stack-queue/app/src/main/java/stack/queue/Node.java
@@ -0,0 +1,34 @@
+package stack.queue;
+
+public class Node <T> {
+    Node next;
+    public T value;
+
+
+    // constructor
+    public Node(T value){
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return value +"";
+    }
+}
+
---
+++ stack-queue/app/src/main/java/stack/queue/Queue.java
@@ -0,0 +1,42 @@
+package stack.queue;
+
+public class Queue <T> {
+    Node <T> front;
+    Node <T> rear;
+    T data ;
+
+
+    public boolean isEmpty(){
+        if(front == null){
+            return true;
+        }
+        return false;
+    }
+    public String peek(){
+        if(isEmpty()){
+            return "the Queue is empty";
+        }
+        return front.value.toString();
+    }
+
+    public void enqueue(T value){
+        Node newNode = new Node(value);
+
+        if(isEmpty())
+            front=newNode;
+        else
+            rear.next = newNode;
+        rear =newNode;
+
+    }
+    public String dequeue(){
+        Node current = front;
+        if(isEmpty()){
+            return "the Queue is empty";
+        }
+        front = front.next ;
+        current.next = null;
+        return current.toString();
+
+    }
+}
