8aab1a3f3e16214c2634031f00ae2829444d75bc
Dima Zeklam <dema.ziklam@gmail.com>
2021-11-22 14:16:13 +0200

    add files

---
+++ TreeIntersection/app/src/main/java/TreeIntersection/Node.java
@@ -0,0 +1,45 @@
+package TreeIntersection;
+
+public class Node<T> {
+    T data;
+    Node<T> left;
+    Node<T>right;
+
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
